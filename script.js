18190c757bfb2af6377222276b0ef71115052f94
GustavoNDC <127903496+GustavoNDC@users.noreply.github.com>
2024-06-18 00:12:57 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const collapsible = document.querySelector(".collapsible");
+    const content = document.querySelector(".content");
+
+    collapsible.addEventListener("click", function() {
+        this.classList.toggle("active");
+        if (content.style.display === "block") {
+            content.style.display = "none";
+        } else {
+            content.style.display = "block";
+        }
+    });
+});
+
+
+function verificarParidade() {
+    const input = document.getElementById("numeroInput").value;
+    const resultado = document.getElementById("resultado");
+    
+    // Lógica para verificar paridade
+    const numero = parseInt(input);
+    const paridade = (numero % 2 === 0) ? "Par" : "Ímpar";
+    resultado.textContent = `O número ${numero} é ${paridade}.`;
+}
